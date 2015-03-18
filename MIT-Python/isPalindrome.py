35d882e64c4cedbb69cf5bd25af9de59e6b51867
wendy <shunw.li@outlook.com>
2015-03-18 21:10:04 +0800

    add fib function

--- MIT-Python/isPalindrome.py
+++ MIT-Python/isPalindrome.py
@@ -10,7 +10,11 @@ def isPalindrome_1(s):
 	else:
 		return s[0]==s[-1] and isPalindrome_1(s[1:-1])
 		
-
+def fib(x):
+	if x==0 or x==1: 
+		return 1
+	else: 
+		return fib(x-1)+fib(x-2)
 
 
 data=str(raw_input("Enter a string: "))
@@ -19,3 +23,4 @@ if isPalindrome_1(data):
 else: 
 	print "This is not Palindrome"
 
+print fib(36)
