76a917203f2f8a203122e9f3d52cf77f049997bc
shunw <shunw.li@outlook.com>
2015-09-15 21:21:49 +0800

    optimize the ver_change coding

--- lang_learn/ver_change.py
+++ lang_learn/ver_change.py
@@ -1,23 +1,30 @@
+# -*- coding: utf-8 -*-
 import random
+import codecs 
 
-fl = open("verb.txt")
+dec = 'utf-8'
+
+handler = codecs.open('data-v.txt', 'r', dec)
 line_counter = 0
 data_dict = dict()
-for line in fl:
+for line in handler:
 	
 	data_row = line.split()
+
 	#-- this is to filter the empty line
 	if len(data_row) < 1: continue
 	
 	#-- get the from_form and to_form index ID.
 	if line_counter == 0:
-		for item in name_field:
-			print item, name_field.index(item)
-		from_form = raw_input('please choose which type is the from_form: ')
-		for item in name_field:
-			if name_field.index(item) == int(from_form): continue
-			print item, name_field.index(item)
-		to_form = raw_input('please choose which type is the to_form: ')
+		Field = data_row
+		for item in data_row:
+			if data_row.index(item) == 0: continue
+			print item, data_row.index(item)
+		from_form = int(raw_input('please choose which type is the from_form: '))
+		for item in data_row:
+			if data_row.index(item) == from_form or data_row.index(item) == 0: continue
+			print item, data_row.index(item)
+		to_form = int(raw_input('please choose which type is the to_form: '))
 	
 	#-- make dict, and prepare the test. 
 	else: 
@@ -28,4 +35,11 @@ for line in fl:
 key_random = data_dict.keys()
 random.shuffle(key_random)
 
-#-- questions
+#-- provide questions
+for q in key_random:
+	ans_opt = raw_input('print "stop" to stop the test\n'+q.encode(dec)+'"'+Field[from_form].encode(dec)+'"'+', please give form '+'"'+Field[to_form].encode(dec)+'": ')
+	if ans_opt == 'stop': break
+	elif ans_opt == data_dict[q]:
+		print 'yes'
+	else: 
+		print 'ans is: ', data_dict[q]
