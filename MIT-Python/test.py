0c240876cd67a63f45b74f0cbf0ea37227e73a62
shunw <shunw.li@outlook.com>
2015-12-30 21:51:20 +0800

    add two language related coding

--- MIT-Python/test.py
+++ MIT-Python/test.py
@@ -1,9 +1,45 @@
 import time
 
-start_time = time.time()
-name = raw_input('what is your name: ')
-end_time = time.time()
-total_time = end_time-start_time
+# start_time = time.time()
+# name = raw_input('what is your name: ')
+# end_time = time.time()
+# total_time = end_time-start_time
 
-print total_time
-print 'It took %0.2f to enter your name' % total_time
+# print total_time
+# print 'It took %0.2f to enter your name' % total_time
+
+class Vec: 
+	def __init__(self, labels, function):
+		self.D = labels
+		self.f = function
+	def func(self):
+		function_set = []
+		for i in self.D:
+			if i in self.f: 
+				function_set.append (self.f[i])
+		return function_set
+
+def setitem(v, d, val): 
+	v.f[d] = val
+
+def list_dot(u, v): return sum([u[i] * v[i] for i in range(len(u))])
+
+def zero_vec(D): return Vec(D, {})
+
+def triangular_solve(rowlist, b):
+	x = zero_vec(rowlist[0].D)
+	for i in reversed(range(len(rowlist))):
+		x[i] = (b[i] - rowlist[i] * x)/rowlist[i][i]
+	return x
+
+def triangular_solve_1(rowlist, label_list, b):
+	x = zero_vec(set(label_list))
+	for r in reversed(range(len(rowlist))):
+		c = label_list[r]
+		x[c] = (b[r] - x*rowlist[r])/rowlist[r][c]
+	return x
+
+if __name__ == '__main__': 
+	u = [1, 2]
+	v = [3, 4]
+	print list_dot(u, v)
--- lang_learn/text.py
+++ lang_learn/text.py
@@ -12,35 +12,28 @@ import word_func
 dec = 'utf-8'
 stop_input = 'stop'
 if __name__=='__main__':
-	sentence = defaultdict(list)
+	txt_j = list()
+	txt_c = list()
 	'''
-	sentence = {'中文意思': [file name, 日文句子]}
+	txt = [(j1, c1), (j2, c2), (j3, c3)]
 	'''
 	#========================================
 	#import all the files from the current dir, the result is a list
 	#========================================
-	files = glob.glob('.'+os.sep+'d*-s*.'+'txt')
+	files = glob.glob('.'+os.sep+'d*-t*.'+'txt')
 	# files = ['data-s8.txt']  # this is for debugging
 	for f in files:
 		handler = codecs.open(f, 'r', dec)
-		counter = 0
 		for line in handler:
-			if counter == 0: 
-				counter += 1
-				continue
 			if len(line.split()) == 0: continue
-			row = line.split('\t')
-			sentence[row[-1]].append(f)
-			sentence[row[-1]].append(row[1])
-	
-	keys = sentence.keys()
-	random.shuffle(keys)
-
-	for k in keys:
-		ans = raw_input(k.encode(dec) + '\nOr please enter "stop" to stop: \n')
+			if line[0] == 'j': txt_j.append(line.split(':')[-1].strip())
+			elif line[0] == 'c': txt_c.append(line.split(':')[-1].strip())
+			
+	for i in range(len(txt_j)):
+		ans = raw_input(txt_c[i].encode(dec) + '\nOr please enter "stop" to stop: \n')
 		if ans == 'stop': break
-		if ans != sentence[k][1].encode(dec):
-			print 'the ans is: %s' % (sentence[k][1].encode(dec))
+		if ans != txt_j[i].encode(dec):
+			print 'the ans is: %s' % (txt_j[i].encode(dec))
 		else: 
 			print 'bingo!'
 
