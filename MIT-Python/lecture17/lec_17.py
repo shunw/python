c3354c1bff1d1d1bc3fe3b4b92f965213168c565
shunw <shunw.li@outlook.com>
2017-02-25 16:17:20 +0800

    complete the lecture coding

--- MIT-Python/lecture17/lec_17.py
+++ MIT-Python/lecture17/lec_17.py
@@ -13,7 +13,7 @@ def get_data(filename):
 			
 		m, n = (line.strip()).split(' ')
 		distance.append(float(m))
-		weight.append(float(n))
+		weight.append(float(n)*9.81)
 	return (distance, weight)
 
 def plot_spring(x_data_list, y_data_list):
@@ -27,16 +27,73 @@ def plot_spr_n_fit(x_data_list, y_data_list, slope, inter):
 	x_data_a = np.array(x_data_list)
 	y_data_a = np.array(y_data_list)
 	fit_data_a = np.array(fit_data)
-	plt.plot(y_data_a, x_data_a, 'bo')
-	plt.plot(fit_data_a, x_data_a)
+	plt.plot(x_data_a, y_data_a, 'bo', label = 'Measured Displacements')
+	plt.plot(x_data_a, fit_data_a, label = 'Linear Fit k = ' + str(1/slope))
+	plt.title('Measured Distance of Spring')
+	plt.xlabel('|Force (Newtons)|')
+	plt.ylabel('Distance (Meters)')
+	plt.legend(loc = 'best')
+	plt.show()
+
+def plot_spr_fit_poly(filename):
+	y_val, x_val = get_data(filename)
+	z = np.polyfit(x_val, y_val, 3)
+	p = np.poly1d(z)
+	fit_val = p(x_val)
+
+	plt.plot(x_val, y_val, 'bo', label = 'Measured Displacements')
+	plt.plot(x_val, fit_val, label = 'Poly Fit')
+	plt.title('Measured Distance of Spring')
+	plt.xlabel('|Force (Newtons)|')
+	plt.ylabel('Distance (Meters')
+	plt.legend(loc = 'best')
+	plt.show()
+
+def get_Trajectory_data(filename):
+	handle = open(filename, 'r')
+	distance = []
+	height1, height2, height3, height4 = [], [], [], []
+	discardheader = handle.readline()
+	for line in handle:
+		d, h1, h2, h3, h4 = line.split()
+		distance.append(float(d)*36.0)
+		height1.append(float(h1))
+		height2.append(float(h2))
+		height3.append(float(h3))
+		height4.append(float(h4))
+	handle.close()
+	return distance, [height1, height2, height3, height4]
+
+def tryFits(filename):
+	distance, heights = get_Trajectory_data(filename)
+	heights = np.matrix(heights)
+	yval = np.mean(heights, axis = 0)
+	yval = yval.tolist()[0]
+
+	plt.title('Trajectory and Its Fit')
+	plt.xlabel('Distance (Inches)')
+	plt.ylabel('Height (Inches)')
+
+	fit_linear = np.poly1d(np.polyfit(distance, yval, 1))
+	y_linear_val = fit_linear(distance)
+
+	fit_poly = np.poly1d(np.polyfit(distance, yval, 3))
+	y_poly_val = fit_poly(distance)
+	
+	plt.plot(distance, yval, 'bo', label = 'Distance and Height')
+	plt.plot(distance, y_linear_val, label = 'Linear Fit')
+	plt.plot(distance, y_poly_val, color = 'r', label = 'Poly Fit w 3')
+
+	plt.legend(loc = 'best')
+
 	plt.show()
 
 
-
 if __name__ == '__main__':
-	dis, weight= get_data("springData.txt")
-	# print type(np.array(weight))
-	m, b = np.polyfit(np.array(dis), np.array(weight), 1)
-	plot_spr_n_fit(dis, weight, m, b)
+	# dis, weight= get_data("springData.txt")
 
-	# stuck in the polyfit thing
+	# slope, inter = np.polyfit(np.array(weight), np.array(dis), 1)
+	# plot_spr_n_fit(weight, dis, slope, inter)
+	# plot_spr_fit_poly('springData.txt')
+	
+	tryFits('launcherData.txt')
---
+++ MIT-Python/lecture17/lec_17_MIT.py
@@ -0,0 +1,103 @@
+import pylab, random
+def getData(fileName):
+    dataFile = open(fileName, 'r')
+    distances = []
+    masses = []
+    discardHeader = dataFile.readline()
+    for line in dataFile:
+        d, m = line.split()
+        distances.append(float(d))
+        masses.append(float(m))
+    dataFile.close()
+    return (masses, distances)
+
+def plotData(fileName):
+    xVals, yVals = getData(fileName)
+    xVals = pylab.array(xVals)
+    yVals = pylab.array(yVals)
+    xVals = xVals*9.81  #acc. due to gravity
+    pylab.plot(xVals, yVals, 'bo', label = 'Measured displacements')
+    pylab.title('Measured Displacement of Spring')
+    pylab.xlabel('|Force| (Newtons)')
+    pylab.ylabel('Distance (meters)')
+
+def fitData(fileName):
+    xVals, yVals = getData(fileName)
+    xVals = pylab.array(xVals)
+    yVals = pylab.array(yVals)
+    xVals = xVals*9.81
+    pylab.plot(xVals, yVals, 'bo', label = 'Measured displacements')
+    pylab.title('Measured Displacement of Spring')
+    pylab.xlabel('|Force| (Newtons)')
+    pylab.ylabel('Distance (meters)')
+    a,b = pylab.polyfit(xVals, yVals, 1)
+    estYVals = a*pylab.array(xVals) + b
+    k = 1/a
+    pylab.plot(xVals, estYVals, label = 'Linear fit, k = '
+               + str(round(k, 5)))
+    pylab.legend(loc = 'best')
+
+def fitData1(fileName):
+    xVals, yVals = getData(fileName)
+    xVals = pylab.array(xVals)
+    yVals = pylab.array(yVals)
+    xVals = xVals*9.81
+    pylab.plot(xVals, yVals, 'bo', label = 'Measured displacements')
+    pylab.title('Measured Displacement of Spring')
+    pylab.xlabel('|Force| (Newtons)')
+    pylab.ylabel('Distance (meters)')
+    a,b = pylab.polyfit(xVals, yVals, 1)
+    estYVals = a*pylab.array(xVals) + b
+    #pylab.plot(xVals, estYVals, label = 'Linear fit')
+    a,b,c,d = pylab.polyfit(xVals, yVals, 3)
+    estYVals = a*(xVals**3) + b*xVals**2 + c*xVals + d
+    pylab.plot(xVals, estYVals, label = 'Cubic fit')
+    pylab.legend(loc = 'best')
+
+def getTrajectoryData(fileName):
+    dataFile = open(fileName, 'r')
+    distances = []
+    heights1, heights2, heights3, heights4 = [],[],[],[]
+    discardHeader = dataFile.readline()
+    for line in dataFile:
+        d, h1, h2, h3, h4 = line.split()
+        distances.append(float(d))
+        heights1.append(float(h1))
+        heights2.append(float(h2))
+        heights3.append(float(h3))
+        heights4.append(float(h4))
+    dataFile.close()
+    return (distances, [heights1, heights2, heights3, heights4])
+
+def tryFits(fName):
+    distances, heights = getTrajectoryData(fName)
+    distances = pylab.array(distances)*36
+    # convert the yds to inches
+    totHeights = pylab.array([0]*len(distances))
+    for h in heights:
+        totHeights = totHeights + pylab.array(h)
+    pylab.title('Trajectory of Projectile (Mean of 4 Trials)')
+    pylab.xlabel('Inches from Launch Point')
+    pylab.ylabel('Inches Above Launch Point')
+    meanHeights = totHeights/float(len(heights))
+    pylab.plot(distances, meanHeights, 'bo')
+    a,b = pylab.polyfit(distances, meanHeights, 1)
+    altitudes = a*distances + b
+    pylab.plot(distances, altitudes, 'r', label = 'Linear Fit')
+    a,b,c = pylab.polyfit(distances, meanHeights, 2)
+    altitudes = a*(distances**2) + b*distances + c
+    pylab.plot(distances, altitudes, 'g', label = 'Quadratic Fit')
+    pylab.legend()
+
+
+
+if __name__ == '__main__':
+    
+    # plotData('springData.txt')
+    # pylab.show()
+
+    # fitData1('springData.txt')
+    # pylab.show()
+
+    tryFits('launcherData.txt')
+    pylab.show()
