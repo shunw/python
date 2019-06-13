afa72fb9046a4241f087c87113e5818c2e252137
Li <shunwen.li@hp.com>
2019-06-13 16:35:48 +0800

    someupdate

--- fluent_p_3/chap_2.py
+++ fluent_p_3/chap_2.py
@@ -108,4 +108,57 @@ for i in range(3):
     board.append(row)
     
 board[1][2] = 'X'
-print (board)
+# print (board)
+
+a = (1, 2, 3)
+# print (id(a))
+b = [4, 5, 6]
+a *= 2
+# print (id(a))
+
+t = (1, 2, [30, 40])
+
+# try: 
+#     t[2] += [50, 60]
+    
+# except:
+#     print (t)
+
+import dis
+
+# print (dis.dis('s[a] += b'))
+
+fruits = ['grape', 'raspberry', 'apple', 'banana']
+# fruits.sort(reverse = True)
+# a = sorted(fruits, key = len)
+# print (a)
+
+'''
+searching w/ bisect
+'''
+
+import bisect
+import sys
+
+HAYSTACK = [1, 4, 5, 6, 8, 12, 15, 20, 21, 23, 23, 26, 29, 30]
+NEEDLES = [0, 1, 2, 5, 8, 10, 22, 23, 29, 30, 31]
+
+ROW_FMT = '{0:2d} @ {1:2d}    {2}{0:<2d}'
+
+def demo(bisect_fn):
+    for needle in reversed(NEEDLES): 
+        position = bisect_fn(HAYSTACK, needle)
+        offset = position * '  |'
+        print (ROW_FMT.format(needle, position, offset))
+
+if __name__ == '__main__':
+    if sys.argv[-1] == 'left':
+        bisect_fn = bisect.bisect_left
+    
+    else: 
+        bisect_fn = bisect.bisect
+
+    print ('DEMO:', bisect_fn.__name__)
+    print ('haystack ->', ' '.join('%2d' % n for n in HAYSTACK))
+    demo(bisect_fn)
+
