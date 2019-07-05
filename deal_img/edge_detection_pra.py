d9526d75a518d0b6e2d060c244aa763bcffefda2
Li <shunwen.li@hp.com>
2019-07-05 12:30:43 +0800

    update conv_forward

--- deal_img/edge_detection_pra.py
+++ deal_img/edge_detection_pra.py
@@ -56,7 +56,11 @@ class try_skimage(object):
         self.fl_name = fl_name
 
         # convert to grey scale
-        self.image = color.rgb2gray(io.imread(self.fl_name))
+        # self.image = color.rgb2gray(io.imread(self.fl_name))
+        self.image = io.imread(self.fl_name)
+
+        self.kernel_sobel = np.array([[1, 0, -1], [2, 0, -2], [1, 0, -1]])
+        self.kernel_scharr = np.array([[3, 0, -3], [10, 0, -10], [3, 0, -3]])
 
         
     
@@ -149,11 +153,78 @@ class try_skimage(object):
 
 
     def final_run(self):
-        self.letter_detect()
+        self.show_ini_image()
+
+def conv_forward(A_prev, W, b, hparameters):
+    """
+    Implements the forward propagation for a convolution function
+    
+    Arguments:
+    A_prev -- output activations of the previous layer, numpy array of shape (m, n_H_prev, n_W_prev, n_C_prev)
+    W -- Weights, numpy array of shape (f, f, n_C_prev, n_C)
+    b -- Biases, numpy array of shape (1, 1, 1, n_C)
+    hparameters -- python dictionary containing "stride" and "pad"
+        
+    Returns:
+    Z -- conv output, numpy array of shape (m, n_H, n_W, n_C)
+    cache -- cache of values needed for the conv_backward() function
+
+    info: 
+    https://github.com/fanghao6666/neural-networks-and-deep-learning/blob/master/py/Convolution%20model%20Step%20by%20Step%20v1.py.html
+    """
+    
+    ### START CODE HERE ###
+    # Retrieve dimensions from A_prev's shape (≈1 line)  
+    (m, n_H_prev, n_W_prev, n_C_prev) = A_prev.shape
+    
+    # Retrieve dimensions from W's shape (≈1 line)
+    (f, f, n_C_prev, n_C) = W.shape
+    
+    # Retrieve information from "hparameters" (≈2 lines)
+    stride = hparameters['stride']
+    pad = hparameters['pad']
+    
+    # Compute the dimensions of the CONV output volume using the formula given above. Hint: use int() to floor. (≈2 lines)
+    n_H = int((n_H_prev -f + 2*pad)/stride) + 1
+    n_W = int((n_W_prev -f + 2*pad)/stride) + 1
+    
+    # Initialize the output volume Z with zeros. (≈1 line)
+    Z = np.zeros((m, n_H, n_W, n_C))
+    
+    # Create A_prev_pad by padding A_prev
+    A_prev_pad = zero_pad(A_prev,pad)
+    
+    for i in range(m):                                 # loop over the batch of training examples
+        a_prev_pad = A_prev_pad[i]                      # Select ith training example's padded activation
+        for h in range(n_W):                           # loop over vertical axis of the output volume
+            for w in range(n_H):                       # loop over horizontal axis of the output volume
+                for c in range(n_C):                   # loop over channels (= #filters) of the output volume
+                    
+                    # Find the corners of the current "slice" (≈4 lines)
+                    vert_start = h
+                    vert_end = h + f
+                    horiz_start = w
+                    horiz_end = w + f
+                    
+                    # Use the corners to define the (3D) slice of a_prev_pad (See Hint above the cell). (≈1 line)
+                    a_slice_prev = a_prev_pad[vert_start:vert_end,horiz_start:horiz_end,:]
+                    
+                    # Convolve the (3D) slice with the correct filter W and bias b, to get back one output neuron. (≈1 line)
+                    Z[i, h, w, c] = conv_single_step(a_slice_prev, W[:,:,:,c], b[:,:,:,c])
+                                        
+    ### END CODE HERE ###
+    
+    # Making sure your output shape is correct
+    assert(Z.shape == (m, n_H, n_W, n_C))
+    
+    # Save information in "cache" for the backprop
+    cache = (A_prev, W, b, hparameters)
+    
+    return Z, cache
 
 if __name__ == '__main__':  
     
-    fl_name = 'vivian.jpg'
+    fl_name = 'light1_part.jpg'
     test = try_skimage(fl_name)
     # test.show_ini_image()
     test.final_run()
