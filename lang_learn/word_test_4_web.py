4c004b8a7f469e456970626dd059227d288f0e0b
shunw <shunw.li@outlook.com>
2019-07-24 19:45:12 +0800

    updat the web code

--- lang_learn/word_test_4_web.py
+++ lang_learn/word_test_4_web.py
@@ -54,7 +54,7 @@ class Prepare_web_jpwords(object):
         self.all_word_df = self.all_word_df.reset_index(drop = True)
         self.all_word_df.to_pickle(self.word_df_name)
 
-    def refine_dataframe(self, get_n_words = 10): 
+    def refine_dataframe(self, get_n_words = 70): 
         '''
         read pkl data
         shuffle the data
@@ -75,7 +75,7 @@ class Prepare_web_jpwords(object):
             
             mean = self.all_word_prepare[self.col_name_mean].iloc[i]
             word = '{w} / {ch} ({les_no}-{wor_no})'.format(w = self.all_word_prepare[self.col_name_word].iloc[i], ch = self.all_word_prepare[self.col_name_chn].iloc[i], les_no = self.all_word_prepare[self.col_name_les].iloc[i], wor_no = self.all_word_prepare[self.col_name_itemno].iloc[i])
-            L = ["- {mean}\n    - {word}\n".format(mean = mean, word = word)]  
+            L = ["- {mean}\n\n    - {word}\n\n".format(mean = mean, word = word)]  
             
             # \n is placed to indicate EOL (End of Line) 
             word_web.writelines(L) 
