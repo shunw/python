9e5cc68ab4bfda0c8c4fc38c4667b24f4274ea2b
shunw <shunw.li@outlook.com>
2019-08-25 20:54:37 +0800

    update to the new part

--- deal_img/grokking_deep_lean.py
+++ deal_img/grokking_deep_lean.py
@@ -10,10 +10,18 @@ toes = np.array([8.5, 9.5, 9.9, 9.0])
 wlrec = np.array([0.65, 0.8, 0.8, 0.9])
 nfans = np.array([1.2, 1.3, 0.5, 1.0])
 
-input = np.array([toes[0],wlrec[0],nfans[0]])
+weights = [[.1, .1, -.3],  # hurt? 
+            [.1, .2, .0],   # wins?
+            [.0, 1.3, .1]   #sad?
+            ]
+weights = np.array(weights)
 input = np.concatenate((toes.reshape(1, 4), wlrec.reshape(1, 4), nfans.reshape(1, 4)), axis=0)
-weights = weights.reshape(1, 3)
+# weights = weights.reshape(1, 3)
 # print (input.shape)
 
 pred = neural_network(input,weights)
 print(pred)
+
+'''
+till predicting on predictions
+'''
