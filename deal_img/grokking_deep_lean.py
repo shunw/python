77758044f7faca25e5414070bdf9d0b163aa90c5
Li <shunwen.li@hp.com>
2019-08-27 16:22:56 +0800

    update the status

--- deal_img/grokking_deep_lean.py
+++ deal_img/grokking_deep_lean.py
@@ -14,14 +14,40 @@ weights = [[.1, .1, -.3],  # hurt?
             [.1, .2, .0],   # wins?
             [.0, 1.3, .1]   #sad?
             ]
+
+
 weights = np.array(weights)
 input = np.concatenate((toes.reshape(1, 4), wlrec.reshape(1, 4), nfans.reshape(1, 4)), axis=0)
 # weights = weights.reshape(1, 3)
 # print (input.shape)
 
 pred = neural_network(input,weights)
-print(pred)
+# print(pred)
 
 '''
-till predicting on predictions
+predicting on predictions
+'''
+
+ih_wgt = [[.1, .2, -.1],  # hid[0]
+            [-.1, .1, .9],  # hid[1]
+            [.1, .4, .1]]   # hid [2]
+
+hp_wgt = [[.3, 1.1, -.3],  # hurt?
+            [.1, .2, .0],  # win? 
+            [.0, 1.3, .1]] # sad?
+
+ih_wgt = np.array(ih_wgt)
+hp_wgt = np.array(hp_wgt)
+weights = [ih_wgt, hp_wgt]
+
+def neural_network(input, weights):
+    hid = weights[0].dot(input)
+    pred = weights[1].dot(hid)
+    return pred
+
+pred = neural_network(input, weights)
+print (pred)
+
+'''
+page 54, hot and cold learning
 '''
