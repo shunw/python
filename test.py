7f2d45520d7b027171d7d2bf21180bad29127900
Li <shunwen.li@hp.com>
2019-10-12 13:16:32 +0800

    update test

--- test.py
+++ test.py
@@ -1,25 +1,6 @@
 import csv
+import matplotlib.pyplot as plt
 
-# -----------------------
-def getAllFieldsData(issue_field_file, issue_part_file):
-	''' @param {issue_field_file} file path
-	    @oaram {issue_part_file}  file path
-	    @return type {field_name, [a,b,c,d]} '''
-	obj = {}
-	field_lines = csv.reader(file(issue_field_file, 'rb'))
-	for fl in field_lines:
-		if (fl[1] != 'Y'): continue
-		obj[fl[0]] = [] # add field as Key
-	part_lines = csv.DictReader(file(issue_part_file,'rb'))
-	for pl in part_lines:
-		for oKey in obj.keys():
-			obj[oKey].append(pl[oKey])
-
-	return obj
-
-if __name__ == '__main__':
-	obj = getAllFieldsData('issue_field.csv', 'D-issue-part.csv')
-	print obj.items()[0][0]
-	#map(int, obj.items()[0][1])
-	#print obj
-
+x = [1, 3, 10, 5, 5, 2, 2, 2, 4, 4]
+plt.hist(x)
+plt.show()
