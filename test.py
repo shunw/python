154c1f2ff69e77869ef8c0994639f67a8a28938d
shunw <shunw.li@outlook.com>
2019-10-30 21:14:28 +0800

    check the confidence level

--- test.py
+++ test.py
@@ -3,99 +3,66 @@ import matplotlib.pyplot as plt
 import scipy.stats as st
 import numpy as np
     
+# sample_1 = np.array([16.869, 25.050, 22.429, 8.456, 20.589, 12.207])
+# compare_2 = np.array([11.074, 9.686, 12.064, 9.351, 8.182, 6.642])
+# # h0: mean_1 <= mean_2; h1: mean_1 > mean_2
+# mean_sp = np.mean(sample_1)
+# mean_cp = np.mean(compare_2)
 
-def mean_2_tail(h0, std, sample_n, sample_mean, alpha = 0.05): 
-    # this is to check mean with two tail
-    # suppose this is normal distribution and according to the given number get the lower and upper data
-    alpha_2 = 1 - alpha/2
-    alpha_1 = alpha/2
-    z_score_upper = st.norm.ppf(alpha_2)
-    z_score_lower = st.norm.ppf(alpha_1)
+# n = len(sample_1)
+# std_sp = np.std(sample_1, ddof = 1)
+# std_cp = np.std(compare_2, ddof = 1)
+
+# t_value = abs(mean_sp - mean_cp)/ (((std_cp ** 2 + std_sp ** 2)/ n) ** .5)
+# t_p = st.t.cdf(t_value, 5.94)
+# # print (mean_cp, mean_sp)
+# print (t_value)
+# print (t_p)
+
+# # check if two sample sets std same
+# sample = np.array([7, -2, 15, 17, 0, -3, 1, 8, 9, -2])
+# compare = np.array([-1, 12, -1, -3, 3, -5, 5, 2, -11, -1, -3])
+
+# sample_n = len(sample)
+# compare_n = len(compare)
+# mean_s = np.mean(sample)
+# mean_c = np.mean(compare)
+# std_s = np.std(sample, ddof = 1)
+# std_c = np.std(compare, ddof = 1)
+
+# F_value = (std_c/ std_s) ** 2
+# F_p = st.f.cdf(F_value, compare_n - 1, sample_n - 1)
+# print (1 - F_p)
+
+from math import sqrt
+ 
+def confidence(clicks, impressions):
+    n = impressions
+    if n == 0: return 0
+    z = 1.96 #1.96 -> 95% confidence
+    phat = float(clicks) / n
+    denorm = 1. + (z*z/n)
+    enum1 = phat + z*z/(2*n)
+    enum2 = z * sqrt(phat*(1-phat)/n + z*z/(4*n*n))
     
-    sample_mean_norm = (sample_mean - h0) / (std / (sample_n ** .5))
-    
-    z_score_p = st.norm.cdf(sample_mean_norm)
-    p_value = 2 * (1-z_score_p)
-    
-    if (sample_mean_norm < z_score_lower) or (sample_mean_norm > z_score_upper): 
-        print ('reject')
-    else: 
-        print ('accept')
-    
-    print ('p_value is: {:.3f}'.format(p_value))
-
-def mean_left_tail(h0, std, sample_n, sample_mean, alpha = .01): 
-    z_score_lower = st.norm.ppf(alpha)
-
-    sample_mean_norm = (sample_mean - h0) / (std / (sample_n ** .5))
-
-    z_score_p = st.norm.cdf(sample_mean_norm)
-    p_value = z_score_p
-
-    if sample_mean_norm < z_score_lower: 
-        print ('reject')
-    else: 
-        print ('accept')
-    
-    print ('p_value is: {:.3f}'.format(p_value))
-    print ('z_value is: {:.3f}'.format(sample_mean_norm))
-
-def mean_right_tail(h0, std, sample_n, sample_mean, alpha = .05):
-    z_score_upper = st.norm.ppf(1 - alpha)
-
-    sample_mean_norm = (sample_mean - h0) / (std / (sample_n ** .5))
-
-    z_score_p = st.norm.cdf(sample_mean_norm)
-    p_value = 1 - z_score_p
-
-    if sample_mean_norm > z_score_upper: 
-        print ('reject')
-    else: 
-        print ('accept')
-    
-    print ('p_value is: {:.3f}'.format(p_value))
-    print ('z_value is: {:.3f}'.format(sample_mean_norm))
-
-if __name__ == '__main__': 
-    # # question 1
-    # h0 = 255
-    # std = 5
-    # sample_n = 40
-    # sample_mean = 255.8
-    # alpha = .05
-
-    # mean_2_tail(h0, std, sample_n, sample_mean)
-
-    # *********************
-    # *********************
-    # parts_data = [1.26, 1.19, 1.31, .97, 1.81, 
-    #                 1.13, .96, 1.06, 1, .94, 
-    #                 .98, 1.1, 1.12, 1.03, 1.16, 
-    #                 1.12, 1.12, 0.95, 1.02, 1.13, 
-    #                 1.23, 0.74, 1.5, 0.5, 0.59, 
-    #                 .99, 1.45, 1.24, 1.01, 2.03, 
-    #                 1.98, 1.97, 0.91, 1.22, 1.06, 
-    #                 1.11, 1.54, 1.08, 1.1, 1.64, 
-    #                 1.7, 2.37, 1.38, 1.6, 1.26, 
-    #                 1.17, 1.12, 1.23, .82, .86]
-    # parts_data = np.array(parts_data)
-    
-    # h0 = 1.35
-    # sample_n = 50
-    # std = np.std(parts_data)
-    # sample_mean = np.mean(parts_data)
-    
-    # mean_left_tail(h0, std, sample_n, sample_mean)
-
-    # *********************
-    # *********************
-    # h0 = 5200
-    # sample_n = 36
-    # sample_mean = 5275
-    # std = 120
-    # alpha = .05
-    # mean_right_tail(h0, std, sample_n, sample_mean)
-
-    # *********************
-    # *********************
-    
+    return (enum1-enum2)/denorm, (enum1+enum2)/denorm
+ 
+def wilson(clicks, impressions):
+    if impressions == 0:
+        return 0
+    else:
+        return confidence(clicks, impressions)
+ 
+if __name__ == '__main__':
+    print (wilson(0,10))
+    # print (wilson(2,10))
+    # print (wilson(20,100))
+    # print (wilson(200,1000))
+ 
+"""    
+--------------------
+results:
+(0.07048879557839793, 0.4518041980521754)
+(0.14384999046998084, 0.27112660859398174)
+(0.1805388068716823, 0.22099327100894336)
+"""
