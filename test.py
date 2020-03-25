e24b746b110cb4f71f5508753524bdb6d779d436
shunw <shunw.li@outlook.com>
2020-03-25 10:43:13 +0800

    optimise some file location

--- test.py
+++ test.py
@@ -1,29 +1,9 @@
-import weibull
-# %matplotlib inline
+# import weibull
+import random
+import numpy as np
 
-fail_times = [
-    42.1, 105.9, 151.3, 195.6,
-    77.8, 117.0, 157.3, 207.0,
-    83.3, 126.9, 163.8, 215.3,
-    88.7, 138.7, 177.2, 217.4,
-    101.8, 148.9, 194.3, 258.8
-]
+# random.seed(3)
 
-suspensions = [1, 0, 1, 1,
-               0, 0, 0, 0,
-               1, 0, 0, 1,
-               1, 0, 1, 0,
-               0, 0, 1, 1]
-
-# this is where the actual analysis and curve fitting occur
-analysis = weibull.Analysis(fail_times, suspensions, unit='hour')
-analysis.fit(method='mle', confidence_level=0.6)
-
-print(analysis.stats)
-
-# analysis.probplot(file_name='gallery-probplot.png')
-
-# analysis.pdf(file_name='gallery-pdf.png')
-# analysis.hazard(file_name='gallery-hazard.png')
-# analysis.sf(file_name='gallery-survival.png')
-# analysis.fr(file_name='gallery-fr.png')
+a = [1, 45, 859, 34]
+b = np.random.choice(a, size = 4, replace = True)
+print (b)
