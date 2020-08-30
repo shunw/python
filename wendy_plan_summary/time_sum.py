8c62b69f1df57bd9dd3d11f67a86b8cdec7cb14e
shunw <shunw.li@outlook.com>
2020-08-30 20:29:20 +0800

    deleted some not used, and added the script to analyze the time

---
+++ wendy_plan_summary/time_sum.py
@@ -0,0 +1,10 @@
+'''
+PURPOSE: this is summary the gap between target and actual practice
+'''
+
+if __name__ == '__main__':
+    # read through the folder and get all the csv data
+
+    # refind the data
+
+    # plot with the data
