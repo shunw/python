b1c6a35e0c0f656924dd4f09871cad42b575d575
shunw <shunw.li@outlook.com>
2020-09-04 13:26:59 +0800

    Add files via upload
    complete the basic game

--- python_cook_book/game_racycar.py
+++ python_cook_book/game_racycar.py
@@ -1,4 +1,6 @@
 import pygame
+import time
+import random
 
 pygame.init()
 
@@ -9,6 +11,8 @@ black = (0, 0, 0)
 white = (255, 255, 255)
 red = (255, 0, 0)
 
+block_color = (53, 115, 255)
+
 car_width = 73
 
 gameDisplay = pygame.display.set_mode((display_width, display_height))
@@ -16,6 +20,14 @@ pygame.display.set_caption('A bit Racey')
 clock = pygame.time.Clock()
 
 carImg = pygame.image.load('car.png')
+
+def things_dodged(count): 
+    font = pygame.font.SysFont(None, 25)
+    text = font.render('Dodged: {}'.format(count), True, black)
+    gameDisplay.blit(text, (0, 0))
+
+def things(thingx, thingy, thingw, thingh, color):
+    pygame.draw.rect(gameDisplay, color, [thingx, thingy, thingw, thingh])
 
 def car(x, y):
     gameDisplay.blit(carImg, (x, y))
@@ -44,35 +56,81 @@ def game_loop():
     y = (display_height * .8)
     
     x_change = 0
+    y_change = 0
 
+    thing_startx = random.randrange(0, display_width)
+    thing_starty = -600
+    thing_speed = 7
+    thing_width = 100
+    thing_height = 100
+
+    thingCount = 1
+    dodged = 0
+    
     gameExit = False
     
     while not gameExit: 
 
         for event in pygame.event.get():
             if event.type == pygame. QUIT:
-                gameExit = True
+                pygame.quit()
+                quit()
             
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_LEFT:
                     x_change = -5
                 if event.key == pygame.K_RIGHT:
                     x_change = 5
+                if event.key == pygame.K_UP:
+                    y_change = -5
+                if event.key == pygame.K_DOWN:
+                    y_change = 5
+                    
             
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
                     x_change = 0
+                if event.key == pygame.K_UP or event.key == pygame.K_DOWN:
+                    y_change = 0
         x += x_change
+        y += y_change
         
 
         gameDisplay.fill(white)
+
+        # things(thingx, thingy, thingw, thingh, color)
+        things(thing_startx, thing_starty, thing_width, thing_height, black)
+        thing_starty += thing_speed
+
         car(x, y)
+        things_dodged(dodged)
 
         if x > display_width - car_width or x < 0: 
-            gameExit = True
+            crash()
         
+        # when the obj is passed through the window, need to restart the obj
+        if thing_starty > display_height: 
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0, display_width)
+            dodged += 1
+            thing_speed += 1
+            thing_width += (dodged * 1.2)
+        
+        if y < thing_starty + thing_height:
+            print ('y crossover')
+            # score += 1
+        
+            if x > thing_startx and x < thing_startx + thing_width or x+car_width > thing_startx and x + car_width < thing_startx+thing_width:
+                print ('x crossover')
+                crash() 
+
+        if thing_starty > display_height:
+            thing_starty = 0 - thing_height
+            thing_startx = random.randrange(0, display_width)
+
         pygame.display.update()
         clock.tick(60)
+
 
 game_loop()
 pygame.quit()
