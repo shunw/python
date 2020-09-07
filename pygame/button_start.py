079e96a286aba05978c8024cfb156eb3b608dae8
shunw <shunw.li@outlook.com>
2020-09-07 13:03:28 +0800

    Add files via upload
    add button start, and try to draw a rect frame around the words

---
+++ pygame/button_start.py
@@ -0,0 +1,39 @@
+import pygame
+
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (255, 0, 0)
+
+display_width = 800
+display_height = 600
+
+pygame.init()
+clock = pygame.time.Clock()
+
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+
+def text_objects(text, font):
+    textSurface = font.render(text, True, black)
+    return textSurface, textSurface.get_rect()
+
+def game_intro():
+    intro = True
+    while intro: 
+        for event in pygame.event.get():
+            # print (event)
+            if event.type == pygame.QUIT: 
+                pygame.quit()
+                quit()
+        
+        gameDisplay.fill(white)
+        pygame.draw.rect(gameDisplay, red, (400, 400, 50, 25))
+        largeText = pygame.font.Font('freesansbold.ttf', 115)
+        TextSurf, TextRect = text_objects('A bit Racey', largeText)
+        TextRect.center = ((display_width/2), (display_height/2))
+        gameDisplay.blit(TextSurf, TextRect)
+        pygame.display.update()
+        clock.tick(15)
+
+game_intro()
+pygame.quit()
+quit()
